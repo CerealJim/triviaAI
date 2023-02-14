73528d2d1e70bc273bcd427af84ab33bfe823413
CerealJim <jim.haz34@gmail.com>
2023-02-14 16:54:22 -0500

    set props value

--- pages/question.tsx
+++ pages/question.tsx
@@ -4,18 +4,21 @@ import { TriviaQuestionProps } from "../types/triviaQueston";
 
 const TriviaQuestion: React.FC<TriviaQuestionProps> = ({
   question,
-  options,
+  options = [],
   answer,
 }) => {
-  console.log(options, "question");
+  const optionsCheck = () => {
+    console.log(options, "options");
+    if (options !== undefined) {
+      return options.map((option, index) => <li key={index}>{option}</li>);
+    }
+  };
+
+  // console.log(options, "options");
   return (
     <div>
       <h2>{question}</h2>
-      <ul>
-        {options.map((option, index) => (
-          <li key={index}>{option}</li>
-        ))}
-      </ul>
+      <ul>{optionsCheck()}</ul>
       <p>Answer: {answer}</p>
     </div>
   );
