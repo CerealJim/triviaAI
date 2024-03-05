fc2c3f8e58c150460d71944739b94721baa27b88
CerealJim <jim.haz34@gmail.com>
2024-03-05 12:02:19 -0500

    building out sections on homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,12 +3,38 @@ import { authOptions } from "./utils/auth";
 import LogoutButton from "./components/LogoutButton";
 import TriviaForm from "./components/form";
 import Link from "next/link";
+import Slider from "./components/Slider";
 import styles from "./styles/Home.module.scss";
+// declare module "react-slick";
+
+// Sample reviews data
+const reviews = [
+  {
+    id: 1,
+    text: "Fantastic service! Loved using Trivia AI for our events.",
+    author: "John Doe",
+  },
+  {
+    id: 2,
+    text: "Incredible features and customization options. Highly recommended.",
+    author: "Jane Smith",
+  },
+  // Add more reviews as needed
+];
 
 // Default function representing the home page
 export default async function Home() {
   // Retrieve the user's session on the server side
   const session = await getServerSession(authOptions);
+
+  // Slider settings
+  const sliderSettings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+  };
 
   return (
     // Main content container with defined styless
@@ -32,6 +58,35 @@ export default async function Home() {
         </div>
       )}
       {/* <TriviaForm /> */}
+      <section className={styles.section}>
+        <p>Generate & Customize trivia games with AI</p>
+        {/* Add video example here */}
+      </section>
+
+      <section className={styles.section}>
+        <h2>Features</h2>
+        <div className={styles.featureCards}>
+          {/* Feature 1 */}
+          <div className={styles.featureCard}>
+            <h3>Feature 1</h3>
+            <p>Description of feature 1.</p>
+          </div>
+          {/* Feature 2 */}
+          <div className={styles.featureCard}>
+            <h3>Feature 2</h3>
+            <p>Description of feature 2.</p>
+          </div>
+          {/* Feature 3 */}
+          <div className={styles.featureCard}>
+            <h3>Feature 3</h3>
+            <p>Description of feature 3.</p>
+          </div>
+        </div>
+      </section>
+      <section className={styles.section}>
+        <h2>Customers cannot stop raving about TriviaAI</h2>
+        <Slider></Slider>
+      </section>
     </main>
   );
 }
